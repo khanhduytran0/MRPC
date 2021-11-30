2043c9437b5988aa3aed3a0c254ef620962542fa
Duy Tran Khanh <khanhduytran0@users.noreply.github.com>
2021-11-30 10:10:34 +0700

    Initial source

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # MRPC
 Experimental Discord Mobile Rich Presence (Android)
+
+## How does it work?
+It's pretty simple.
+- Connect to the [Discord Gateway](https://discord.com/developers/docs/topics/gateway) as a normal Discord Client.
+- Send [Identify](https://discord.com/developers/docs/topics/gateway#identifying) and [Update Presence](https://discord.com/developers/docs/topics/gateway#update-presence).
+
+## Notes
+- I haven't made this as a background service yet.
+- Rate limiting is not yet handled.
+- This app uses the Discord Gateway instead of OAuth2 API (which I can't even find the documentation for the `activities.write` scope), so this might not be safe. Use this at your own risk.
+- Zlib compression is not yet used, due to the current using library doesn't correctly handle it(?).
+
+## License
+[Apache License 2.0](https://github.com/khanhduytran0/MRPC/blob/main/LICENSE).
+
+## Third party libraries and their licenses
+- [gson](https://github.com/google/gson): [Apache License 2.0](https://github.com/google/gson/blob/master/LICENSE).
+- [Java-WebSocket](https://github.com/TooTallNate/Java-WebSocket): [MIT License](https://github.com/TooTallNate/Java-WebSocket/blob/master/LICENSE).
+Note that an old version (1.3.9) is used instead of latest due to the dependence of slf4j. I will update it later.
---
+++ app/src/main/java/com/kdt/mrpc/MainActivity.java
@@ -0,0 +1,423 @@
+package com.kdt.mrpc;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.ArrayMap;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.util.Map;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+public class MainActivity extends Activity 
+{
+    private SharedPreferences pref;
+
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    private Runnable heartbeatRunnable;
+    private WebSocketClient webSocketClient;
+    private Thread heartbeatThr, wsThr;
+    private int heartbeat_interval, seq;
+    private String authToken;
+
+    private WebView webView;
+    private TextView textviewLog;
+    private Button buttonConnect, buttonSetActivity;
+    private EditText editActivityName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        heartbeatRunnable = new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    appendlnToLog("Heartbeat wait for " + heartbeat_interval);
+                    if (heartbeat_interval < 10000) throw new RuntimeException("invalid");
+                    Thread.sleep(heartbeat_interval);
+                    webSocketClient.send(/*encodeString*/("{\"op\":1, \"d\":" + (seq==0?"null":Integer.toString(seq)) + "}"));
+                    appendlnToLog("Heartbeat sent");
+                } catch (InterruptedException e) {}
+            }
+        };
+
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        webView = (WebView) findViewById(R.id.mainWebView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView v, String url) {
+                Log.d("Web", "Attempt to enter " + url);
+                webView.stopLoading();
+                if (url.endsWith("/app")) {
+                    webView.setVisibility(View.GONE);
+                    extractToken();
+                    login(v);
+                }
+                return false;
+                // super.shouldOverrideUrlLoading(v, url);
+            }
+        });
+
+        textviewLog = (TextView) findViewById(R.id.textviewLog);
+        buttonConnect = (Button) findViewById(R.id.buttonConnect);
+        buttonSetActivity = (Button) findViewById(R.id.buttonSetActivity);
+        buttonSetActivity.setEnabled(false);
+        editActivityName = (EditText) findViewById(R.id.editActivityName);
+        heartbeatThr = new Thread(heartbeatRunnable);
+    }
+    /*
+     @Override
+     protected void onResume() {
+     super.onResume();
+     Uri data = getIntent().getData(); 
+     if (data != null && wsThr == null) {
+     if (data.toString().contains("access_token=")) {
+     accessToken = data.toString().substring(
+     data.toString().indexOf("access_token=") + 13,
+     data.toString().indexOf("&expires_in")
+     );
+     actualConnect(null);
+     } else {
+     actualConnect(data.getQueryParameter("code"));
+     }
+     }
+     }
+     */
+
+    public void sendPresenceUpdate(View v) {
+        ArrayMap<String, Object> presence = new ArrayMap<>();
+
+        if (editActivityName.getText().toString().isEmpty()) {
+            presence.put("activities", new Object[]{});
+        } else {
+            ArrayMap<String, Object> activity = new ArrayMap<>();
+            activity.put("name", editActivityName.getText().toString());
+            activity.put("type", 0);
+
+            ArrayMap<String, Object> timestamps = new ArrayMap<>();
+            timestamps.put("start", System.currentTimeMillis());
+
+            activity.put("timestamps", timestamps);
+            presence.put("activities", new Object[]{activity});
+        }
+
+        presence.put("afk", true);
+        presence.put("since", 0);
+        presence.put("status", "online");
+
+        ArrayMap<String, Object> arr = new ArrayMap<>();
+        arr.put("op", 3);
+        arr.put("d", presence);
+
+        webSocketClient.send(gson.toJson(arr));
+    }
+
+    private void sendIdentify() {
+        ArrayMap<String, Object> prop = new ArrayMap<>();
+        prop.put("$os", "linux");
+        prop.put("$browser", "Discord Android");
+        prop.put("$device", "unknown");
+
+        ArrayMap<String, Object> data = new ArrayMap<>();
+        data.put("token", authToken);
+        data.put("properties", prop);
+        data.put("compress", false);
+        data.put("intents", 0);
+
+        ArrayMap<String, Object> arr = new ArrayMap<>();
+        arr.put("op", 2);
+        arr.put("d", data);
+
+        webSocketClient.send(gson.toJson(arr));
+    }
+
+    private void createWebSocketClient() {
+        appendlnToLog("Connecting...");
+
+        URI uri;
+        try {
+            uri = new URI("wss://gateway.discord.gg/?encoding=json&v=9"); // &compress=zlib-stream");
+        }
+        catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        ArrayMap<String, String> headerMap = new ArrayMap<>();
+        //headerMap.put("Accept-Encoding", "gzip");
+        //headerMap.put("Content-Type", "gzip");
+
+        webSocketClient = new WebSocketClient(uri, headerMap) {
+            @Override
+            public void onOpen(ServerHandshake s) {
+                appendlnToLog("Connection opened");
+            }
+
+            @Override
+            public void onMessage(ByteBuffer message) {
+                // onMessage(decodeString(message.array()));
+            }
+
+            @Override
+            public void onMessage(String message) {
+                // appendlnToLog("onTextReceived: " + message);
+
+                ArrayMap<String, Object> map = gson.fromJson(
+                    message, new TypeToken<ArrayMap<String, Object>>() {}.getType()
+                );
+
+                // obtain sequence number
+                Object o = map.get("s");
+                if (o != null) {
+                    seq = ((Double)o).intValue();
+                }
+
+                int opcode = ((Double)map.get("op")).intValue();
+                switch (opcode) {
+                    case 0: // Dispatch event
+                        if (((String)map.get("t")).equals("READY")) {
+                            appendlnToLog("Received READY event");
+                            Map data = (Map) ((Map)map.get("d")).get("user");
+                            appendlnToLog("Connected to " + data.get("username") + "#" + data.get("discriminator"));
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    buttonSetActivity.setEnabled(true);
+                                }
+                            });
+                            return;
+                        }
+                        break;
+                    case 10: // Hello
+                        Map data = (Map) map.get("d");
+                        heartbeat_interval = ((Double)data.get("heartbeat_interval")).intValue();
+                        heartbeatThr.start();
+                        sendIdentify();
+                        break;
+                    case 1: // Heartbeat request
+                        if (!heartbeatThr.interrupted()) {
+                            heartbeatThr.interrupt();
+                        }
+                        webSocketClient.send(/*encodeString*/("{\"op\":1, \"d\":" + (seq==0?"null":Integer.toString(seq)) + "}"));
+
+                        break;
+                    case 11: // Heartbeat ACK
+                        if (!heartbeatThr.interrupted()) {
+                            heartbeatThr.interrupt();
+                        }
+                        heartbeatThr = new Thread(heartbeatRunnable);
+                        heartbeatThr.start();
+                        break;
+                }
+                //appendlnToLog("Received op " + opcode + ": " + message);
+            }
+
+            @Override
+            public void onClose(int code, String reason, boolean remote) {
+                appendlnToLog("Connection closed with exit code " + code + " additional info: " + reason + "\n");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        buttonConnect.setText("Connect");
+                        buttonSetActivity.setEnabled(false);
+                    }
+                });
+                throw new RuntimeException("Interrupt");
+            }
+
+            @Override
+            public void onError(Exception e) {
+                if (!e.getMessage().equals("Interrupt")) {
+                    appendlnToLog(Log.getStackTraceString(e));
+                }
+            }
+        };
+        webSocketClient.connect();
+    }
+
+    public void appendlnToLog(final String msg) {
+        runOnUiThread(new Runnable(){
+            @Override
+            public void run() {
+                textviewLog.append(msg + "\n");
+            }
+        });
+    }
+
+    public void login(View v) {
+        if (webView.getVisibility() == View.VISIBLE) {
+            webView.stopLoading();
+            webView.setVisibility(View.GONE);
+            return;
+        }
+        if (authToken != null) {
+            appendlnToLog("Logged in");
+            if (v == null) {
+                actualConnect();
+                return;
+            }
+        }
+        webView.setVisibility(View.VISIBLE);
+        webView.loadUrl("https://discord.com/login");
+    }
+
+    public void connect(View v) {
+        if (!extractToken()) {
+            Toast.makeText(this, "Token is not found, opening login page", Toast.LENGTH_SHORT).show();
+            login(null);
+        } else {
+            actualConnect();
+        }
+    }
+
+    public void actualConnect() {
+        if (buttonConnect.getText().equals("Connect")) {
+            wsThr = new Thread(new Runnable(){
+                @Override
+                public void run() {
+                    createWebSocketClient();
+                }
+            });
+            buttonConnect.setText("Disconnect");
+            wsThr.start();
+        } else {
+            heartbeatThr.interrupt();
+            webSocketClient.close(1000);
+            wsThr.interrupt();
+            buttonConnect.setText("Connect");
+            buttonSetActivity.setEnabled(false);
+        }
+    }
+
+    public boolean extractToken() {
+        // ~~extract token in an ugly way :troll:~~
+        try {
+            File f = new File(getFilesDir().getParentFile(), "app_webview/Default/Local Storage/leveldb");
+            File[] fArr = f.listFiles(new FilenameFilter(){
+                @Override
+                public boolean accept(File file, String name) {
+                    return name.endsWith(".log");
+                }
+            });
+            if (fArr.length == 0) {
+                return false;
+            }
+            f = fArr[0];
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.contains("token")) {
+                    break;
+                }
+            }
+            line = line.substring(line.indexOf("token") + 5);
+            line = line.substring(line.indexOf("\"") + 1);
+            authToken = line.substring(0, line.indexOf("\""));
+            return true;
+        } catch (Throwable e) {
+            appendlnToLog("Failed extracting token: " + Log.getStackTraceString(e));
+            return false;
+        }
+    }
+
+    /*
+     public void code2AccessToken(String code) {
+     try {
+     HttpURLConnection conn = (HttpURLConnection) new URL("https://discord.com/api/oauth2/token").openConnection();
+     conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+     conn.setDoInput(true);
+
+     ArrayMap<String, Object> prop = new ArrayMap<>();
+     prop.put("client_id", "591317049637339146");
+     prop.put("client_secret", "Discord Android");
+     prop.put("grant_type", "authorization_code");
+     prop.put("code", code);
+     prop.put("redirect_url", "https://account.samsung.com/accounts/oauth/callback");
+
+     byte[] arr = gson.toJson(prop).getBytes();
+
+     conn.getOutputStream().write(arr, 0, arr.length);
+     } catch (Throwable e) {
+     throw new RuntimeException(e);
+     }
+     }
+
+    public String decodeString(byte[] arr) {
+        Inflater i = new Inflater();
+        i.setInput(arr, 0, arr.length);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(arr.length); 
+        byte[] buf = new byte[1024];
+        try {
+            int count = 1;
+            while (!i.finished() && count > 0)  { 
+                count = i.inflate(buf);
+                appendlnToLog("decode " + count);
+                bos.write(buf, 0, count); 
+            }
+        } catch (DataFormatException e) {
+            throw new RuntimeException(e);
+        }
+        i.end();
+        try {
+            bos.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new String(bos.toByteArray());
+    }
+
+    public byte[] encodeString(String str) {
+        byte[] arr = str.getBytes();
+        Deflater compresser = new Deflater();
+        compresser.setInput(arr);
+        compresser.finish();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(arr.length); 
+        // Compress the data 
+        byte[] buf = new byte[1024]; 
+        while (!compresser.finished())  { 
+            int count = compresser.deflate(buf); 
+            bos.write(buf, 0, count);
+        } 
+        compresser.end();
+        try {
+            bos.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        
+        return bos.toByteArray();
+    }
+ */
+}
